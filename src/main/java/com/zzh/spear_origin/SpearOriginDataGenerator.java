4a8310e2d50b0fc217b16c047e58e2f00e95ddfb
邹梓航 <zouzihang2003@gmail.com>
2026-02-10 11:54:55 +0800

    枪道：归元

---
+++ src/main/java/com/zzh/spear_origin/SpearOriginDataGenerator.java
@@ -0,0 +1,11 @@
+package com.zzh.spear_origin;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpearOriginDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
