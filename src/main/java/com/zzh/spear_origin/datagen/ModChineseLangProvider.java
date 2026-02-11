4f1642b0eea33d814ec0459fce8d246e6418597a
ZZH-MAC <zouzihang2003@gmail.com>
2026-02-11 11:07:23 +0800

    完善语言数据生成

--- src/main/java/com/zzh/spear_origin/datagen/ModChineseLangProvider.java
+++ src/main/java/com/zzh/spear_origin/datagen/ModChineseLangProvider.java
@@ -25,5 +25,29 @@ public class ModChineseLangProvider extends FabricLanguageProvider {
 
         // 物品组
         translationBuilder.add("itemGroup.spear_origin", "枪道：归元");
+
+        //翻译模版
+        translationBuilder.add(ModItems.WOOD_TO_STONE_TEMPLATE, "磐石重铸");
+        translationBuilder.add(ModItems.STONE_TO_COPPER_TEMPLATE, "赤铜冶炼");
+        translationBuilder.add(ModItems.COPPER_TO_IRON_TEMPLATE, "百炼成钢");
+        translationBuilder.add(ModItems.IRON_TO_GOLD_TEMPLATE, "黄金律法");
+        translationBuilder.add(ModItems.GOLD_TO_DIAMOND_TEMPLATE, "璀璨星陨");
+        translationBuilder.add(ModItems.DIAMOND_TO_NETHERITE_TEMPLATE, "极境余烬");
+
+        //翻译适用于...
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_wood_spear","适用木枪");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_stone_spear","适用石枪");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_copper_spear","适用铜枪");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_iron_spear","适用铁枪");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_gold_spear","适用金枪");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_diamond_spear","适用钻石枪");
+
+        //翻译材料...
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_stone","所需材料：石");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_copper","所需材料：铜");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_iron","所需材料：铁");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_gold","所需材料：金");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_diamond","所需材料：钻石");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_netherite","所需材料：下界合金");
     }
 }
--- src/main/java/com/zzh/spear_origin/datagen/ModEnglishLangProvider.java
+++ src/main/java/com/zzh/spear_origin/datagen/ModEnglishLangProvider.java
@@ -28,7 +28,28 @@ public class ModEnglishLangProvider extends FabricLanguageProvider {
         translationBuilder.add("itemGroup.spear_origin", "Spear: Origin");
 
         //翻译模版
-        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_wood_spear","Applies to Wood Spear");
+        translationBuilder.add(ModItems.WOOD_TO_STONE_TEMPLATE,"Rock Solid Reforging");
+        translationBuilder.add(ModItems.STONE_TO_COPPER_TEMPLATE,"Copper Smelting");
+        translationBuilder.add(ModItems.COPPER_TO_IRON_TEMPLATE,"Tempered Steel");
+        translationBuilder.add(ModItems.IRON_TO_GOLD_TEMPLATE,"Golden Law");
+        translationBuilder.add(ModItems.GOLD_TO_DIAMOND_TEMPLATE,"Stellar Brilliance");
+        translationBuilder.add(ModItems.DIAMOND_TO_NETHERITE_TEMPLATE,"Primal Embers");
 
+        //翻译模版适用于...
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_wood_spear","Applies to Wood Spear");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_stone_spear","Applies to Stone Spear");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_copper_spear","Applies to Copper Spear");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_iron_spear","Applies to Iron Spear");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_gold_spear","Applies to Gold Spear");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_diamond_spear","Applies to Diamond Spear");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.applies_to_netherite_spear","Applies to Netherite Spear");
+
+        //材料...
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_stone","Ingredients: Stone");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_copper","Ingredients: Copper");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_iron","Ingredients: Iron");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_gold","Ingredients: Gold");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_diamond","Ingredients: Diamond");
+        translationBuilder.add("item.spear_origin.smithing_template.wood_upgrade.ingredients_netherite","Ingredients: Netherite");
     }
 }
